604a137b819a37817a6ea4108873db0bfc783d39
leocalmeida <leonardocaetanoalmeida@gmail.com>
2020-04-23 08:08:43 -0300

    Correcoes nos Arquivos DashboardControllerCopy e SessionControllerCopy. Adicao dos arquivos de config Editor e eslintrc

--- src/Controllers/SessionControllerCopy.js
+++ src/Controllers/SessionControllerCopy.js
@@ -6,7 +6,11 @@ module.exports = {
   async store(request, response) {
     const userLogin = request.body;
 
+    console.log(userLogin);
+
     const userValidate = await User.findOne({ email: userLogin.email });
+
+    console.log(userValidate);
 
     if (!userValidate) {
       return response.json("E-mail não cadastrado");
@@ -17,10 +21,13 @@ module.exports = {
       userValidate.password
     );
 
+    console.log(passwordValidate);
+
     if (!passwordValidate) {
       return response.json("Senha inválida");
     }
 
+    console.log(userValidate);
     return response.json(userValidate);
   },
 };
