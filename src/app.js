5704f9cd0f7901886646bf0413a89710808f54d2
leocalmeida <leonardocaetanoalmeida@gmail.com>
2020-05-19 22:39:06 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const routes = require("./routes");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 require("dotenv").config();
 
@@ -13,6 +14,7 @@ mongoose.connect(MONGO_CONNECTION, {
   useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
